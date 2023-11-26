34a4db5d39eb5c187f1047159de6793e4bfc85f3
kholoud001 <kholoud.sanak@gmail.com>
2023-11-26 20:37:44 +0100

    admin form style

--- pages/pages/admin/admin_signin.php
+++ pages/pages/admin/admin_signin.php
@@ -5,7 +5,6 @@ session_start();
 require_once '../../config/tables.php';
 require_once '../../config/config.php';
 
-
 // Create an instance of the TableCreator class
 $tableCreator = new TableCreator($conn);
 
@@ -26,7 +25,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             exit();
         } else {
             // Failed signin, display an error message
-            echo "Invalid username or password.";
+            $errorMessage = "Invalid username or password.";
         }
     }
 }
@@ -37,18 +36,33 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Signup and Signin</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css">
 </head>
-<body>
-<h2>Admin Signin</h2>
-<form method="post" action="admin_signin.php">
-    <label for="username">Username:</label>
-    <input type="text" name="username" required><br>
+<body class="bg-gray-100 min-h-screen flex items-center justify-center">
 
-    <label for="password">Password:</label>
-    <input type="password" name="password" required><br>
+<div class="bg-white p-8 rounded-md shadow-md w-1/3">
 
-    <input type="submit" name="signin" value="Sign In">
-</form>
+    <h2 class="text-2xl mb-4 text-center">Admin Signin</h2>
+
+    <?php if (isset($errorMessage)) : ?>
+        <p class="text-red-500 mb-4"><?php echo $errorMessage; ?></p>
+    <?php endif; ?>
+
+    <form method="post" action="admin_signin.php" class="space-y-4">
+        <div>
+            <label for="username" class="block text-sm font-medium text-gray-600">Username:</label>
+            <input type="text" name="username" required class="w-full px-4 py-2 border rounded-md">
+        </div>
+
+        <div>
+            <label for="password" class="block text-sm font-medium text-gray-600">Password:</label>
+            <input type="password" name="password" required class="w-full px-4 py-2 border rounded-md">
+        </div>
+
+        <button type="submit" name="signin" class="w-full bg-blue-500 text-white px-4 py-2 rounded-md">Sign In</button>
+    </form>
+
+</div>
+
 </body>
 </html>
-
--- pages/pages/admin/admin_signup.php
+++ pages/pages/admin/admin_signup.php
@@ -13,7 +13,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $email = $_POST['email'];
     $phone = $_POST['phone'];
 
-
     // Create an instance of the TableCreator class
     $tableCreator = new TableCreator($conn);
 
@@ -35,22 +34,39 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Signup and Signin</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css">
 </head>
-<body>
-<h2>Admin Signup</h2>
-<form method="post" action="admin_signup.php">
-    <label for="username">Username:</label>
-    <input type="text" name="username" required><br>
+<body class="bg-gray-100 min-h-screen flex items-center justify-center">
 
-    <label for="password">Password:</label>
-    <input type="password" name="password" required><br>
+<div class="bg-white p-8 rounded-md shadow-md w-1/3">
 
-    <label for="email">Email:</label>
-    <input type="email" name="email" required><br>
+    <h2 class="text-2xl mb-4 text-center">Admin Signup</h2>
 
-    <label for="phone">Phone:</label>
-    <input type="tel" name="phone" required><br>
+    <form method="post" action="admin_signup.php" class="space-y-4">
+        <div>
+            <label for="username" class="block text-sm font-medium text-gray-600">Username:</label>
+            <input type="text" name="username" required class="w-full px-4 py-2 border rounded-md">
+        </div>
 
-    <input type="submit" name="signup" value="Sign Up">
-</form>
+        <div>
+            <label for="password" class="block text-sm font-medium text-gray-600">Password:</label>
+            <input type="password" name="password" required class="w-full px-4 py-2 border rounded-md">
+        </div>
 
+        <div>
+            <label for="email" class="block text-sm font-medium text-gray-600">Email:</label>
+            <input type="email" name="email" required class="w-full px-4 py-2 border rounded-md">
+        </div>
+
+        <div>
+            <label for="phone" class="block text-sm font-medium text-gray-600">Phone:</label>
+            <input type="tel" name="phone" required class="w-full px-4 py-2 border rounded-md">
+        </div>
+
+        <button type="submit" name="signup" class="w-full bg-blue-500 text-white px-4 py-2 rounded-md">Sign Up</button>
+    </form>
+
+</div>
+
+</body>
+</html>
