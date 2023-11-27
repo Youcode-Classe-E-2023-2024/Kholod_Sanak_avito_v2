8e05e4d097b33875ca059f31303d83b0ff9bbf3a
kholoud001 <kholoud.sanak@gmail.com>
2023-11-27 14:51:22 +0100

    admin (add user)

--- pages/admin/admin_add.php
+++ pages/admin/admin_add.php
@@ -35,24 +35,25 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['addRegularUser'])) {
 <body class="p-8">
 
 <h3 class="mt-8 mb-4 text-xl">Add New Regular User</h3>
-<form method="post" action="admin_add.php">
+<form method="post" action="admin_add.php" class="w-full max-w-md bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
     <!-- Add form fields for new regular user -->
-    <label for="newUsername" class="block">Username:</label>
-    <input type="text" name="newUsername" required class="w-full border p-2 mb-2">
+    <label for="newUsername" class="block text-sm font-medium text-gray-600">Username:</label>
+    <input type="text" name="newUsername" required class="w-full px-4 py-2 mb-2 border focus:outline-none focus:shadow-outline">
 
-    <label for="newEmail" class="block">Email:</label>
-    <input type="email" name="newEmail" required class="w-full border p-2 mb-2">
+    <label for="newEmail" class="block text-sm font-medium text-gray-600">Email:</label>
+    <input type="email" name="newEmail" required class="w-full px-4 py-2 mb-2 border focus:outline-none focus:shadow-outline">
 
-    <label for="newPassword" class="block">Password:</label>
-    <input type="password" name="newPassword" required class="w-full border p-2 mb-2">
+    <label for="newPassword" class="block text-sm font-medium text-gray-600">Password:</label>
+    <input type="password" name="newPassword" required class="w-full px-4 py-2 mb-2 border focus:outline-none focus:shadow-outline">
 
-    <label for="newPhone" class="block">Phone:</label>
-    <input type="text" name="newPhone" required class="w-full border p-2 mb-2">
+    <label for="newPhone" class="block text-sm font-medium text-gray-600">Phone:</label>
+    <input type="text" name="newPhone" required class="w-full px-4 py-2 mb-2 border focus:outline-none focus:shadow-outline">
 
-    <input type="submit" name="addRegularUser" value="Add User" class="bg-blue-500 text-white px-4 py-2 rounded">
+    <input type="submit" name="addRegularUser" value="Add User" class="bg-blue-500 hover:bg-blue-700 text-white px-4 py-2 rounded focus:outline-none focus:shadow-outline">
 </form>
-<a href="admin_logout.php" class="mt-8 text-blue-500 hover:underline">Logout</a>
-<a href="admin_dashboard.php" > Return to dashboard </a>
+
+<a href="admin_dashboard.php" class="text-blue-500 hover:underline inline-block"> Return to dashboard  </a>
+<a href="admin_logout.php" class=" text-red-500 hover:underline">Logout</a>
 
 </body>
 </html>
