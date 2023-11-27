3f3d1e7d8d06698fbe54d5f1d1f7e8a679bfb0bd
kholoud001 <kholoud.sanak@gmail.com>
2023-11-27 14:51:35 +0100

    admin (modify user)

--- pages/admin/admin_modify.php
+++ pages/admin/admin_modify.php
@@ -35,25 +35,28 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['modifyRegularUser']))
 <body class="p-8">
 
 <h3 class="mt-8 mb-4 text-xl">Modify User</h3>
-<form method="post" action="admin_modify.php">
+<form method="post" action="admin_modify.php" class="w-full max-w-md bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
     <!-- Add form fields for modifying a regular user -->
-    <label for="userIdToModify" class="block">User ID to Modify:</label>
-    <input type="text" name="userIdToModify" required class="w-full border p-2 mb-2">
+    <label for="userIdToModify" class="block text-sm font-medium text-gray-600">User ID to Modify:</label>
+    <input type="text" name="userIdToModify" required class="w-full px-4 py-2 mb-2 border focus:outline-none focus:shadow-outline">
 
-    <label for="newUsername" class="block">New Username:</label>
-    <input type="text" name="newUsername" required class="w-full border p-2 mb-2">
+    <label for="newUsername" class="block text-sm font-medium text-gray-600">New Username:</label>
+    <input type="text" name="newUsername" required class="w-full px-4 py-2 mb-2 border focus:outline-none focus:shadow-outline">
 
-    <label for="newEmail" class="block">New Email:</label>
-    <input type="email" name="newEmail" required class="w-full border p-2 mb-2">
+    <label for="newEmail" class="block text-sm font-medium text-gray-600">New Email:</label>
+    <input type="email" name="newEmail" required class="w-full px-4 py-2 mb-2 border focus:outline-none focus:shadow-outline">
 
-    <label for="newPhone" class="block">New Phone:</label>
-    <input type="text" name="newPhone" required class="w-full border p-2 mb-2">
+    <label for="newPhone" class="block text-sm font-medium text-gray-600">New Phone:</label>
+    <input type="text" name="newPhone" required class="w-full px-4 py-2 mb-2 border focus:outline-none focus:shadow-outline">
 
-    <input type="submit" name="modifyRegularUser" value="Modify Regular User" class="bg-blue-500 text-white px-4 py-2 rounded">
+    <input type="submit" name="modifyRegularUser" value="Modify Regular User" class="bg-blue-500 hover:bg-blue-700 text-white px-4 py-2 rounded focus:outline-none focus:shadow-outline">
 </form>
-<a href="admin_logout.php" class="mt-8 text-blue-500 hover:underline">Logout</a>
 
-<a href="admin_dashboard.php" > Return to dashboard </a>
+<a href="admin_dashboard.php" class="text-blue-500 hover:underline inline-block"> Return to dashboard  </a>
+<a href="admin_logout.php" class=" text-red-500 hover:underline">Logout</a>
+
+
+
 </body>
 </html>
 
